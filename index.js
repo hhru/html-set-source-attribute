04c2b20194ec1ca5e41b58ac13b7df23444126eb
eugenkov <kovalyov.eugene@gmail.com>
2017-01-11 17:08:26 +0300

    Initial commit

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ module.exports = function(fileContent, path) {
 
     var storage = [];
 
-    var content = fileContent.replace(/<!--([\s\S]+?)-->/gm, function(match, contents) {
-        var newlinesCount = (contents.match(/\n/g) || []).length;
+    var content = fileContent.replace(/<!--([\s\S]+?)-->/gm, function(match, p1) {
+        var newlinesCount = (p1.match(/\n/g) || []).length;
         var output = '<!--##' + storage.length + (new Array(newlinesCount + 1)).join('\n') + '##-->';
-        storage.push(contents);
+        storage.push(p1);
         return output;
     });
 
     var lines = content.split('\n');
     content = lines.map(function(line, lineNumber) {
 
-        return line.replace(/<(?!xsl:|func:)[a-z0-9_:]+(?=\s|\/|>)/ig, function(node) {
-            return node + ' source="' + path + ':' + (lineNumber + 1) + '"';
+        return line.replace(/<(?!xsl:|func:)[a-z0-9_:]+(?=\s|\/|>)/ig, function(match) {
+            return match + ' source="' + path + ':' + (lineNumber + 1) + '"';
         });
-        
+
     }).join('\n');
 
-    content = content.replace(/<!--##(\d+)\n*##-->/g, function(match, contents) {
-        return '<!--' + storage[contents] + '-->';
+    content = content.replace(/<!--##(\d+)\n*##-->/g, function(match, p1) {
+        return '<!--' + storage[p1] + '-->';
     });
 
     return content;
